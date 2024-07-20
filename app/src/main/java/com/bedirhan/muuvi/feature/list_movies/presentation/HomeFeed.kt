a5ef3d3da3cff69b9680decb6860f9c30cbf4577
Bedirhan Tong <bedirhantongdev@gmail.com>
2024-07-20 19:20:40 +0300

    code reformatting

--- app/src/main/java/com/bedirhan/muuvi/feature/list_movies/presentation/HomeFeed.kt
+++ app/src/main/java/com/bedirhan/muuvi/feature/list_movies/presentation/HomeFeed.kt
@@ -6,9 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bedirhan.muuvi.databinding.FragmentHomeFeedBinding
-import com.bedirhan.muuvi.feature.list_movies.presentation.adapter.HomeFeedPopularMovieRecyclerAdapter
+import com.bedirhan.muuvi.feature.list_movies.presentation.adapter.HomeFeedMovieRecyclerAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFeed : Fragment() {
@@ -16,11 +20,15 @@ class HomeFeed : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: HomeFeedViewModel by viewModels()
-    private val recyclerAdapter: HomeFeedPopularMovieRecyclerAdapter by lazy {
-        HomeFeedPopularMovieRecyclerAdapter()
+    private val topRatedMoviesRecyclerAdapter: HomeFeedMovieRecyclerAdapter by lazy {
+        HomeFeedMovieRecyclerAdapter()
     }
-
-
+    private val popularMoviesRecyclerAdapter: HomeFeedMovieRecyclerAdapter by lazy {
+        HomeFeedMovieRecyclerAdapter()
+    }
+    private val upcomingMoviesRecyclerAdapter: HomeFeedMovieRecyclerAdapter by lazy {
+        HomeFeedMovieRecyclerAdapter()
+    }
 
 
     override fun onCreateView(
@@ -28,18 +36,51 @@ class HomeFeed : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeFeedBinding.inflate(inflater, container, false)
+        setupRecyclerViews()
+        observeMovies()
+        collectApiRequest()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerViews()
+        observeMovies()
+        collectApiRequest()
+    }
 
-        binding.recyclerviewHomeFeed.adapter = recyclerAdapter
-        viewModel.moviesLiveData.observe(viewLifecycleOwner) { articles ->
-            recyclerAdapter.submitList(articles)
+    private fun collectApiRequest() = binding.apply {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.getPopularMovies()
+                    viewModel.getTopRatedMovies()
+                    viewModel.getUpcomingMovies()
+                }
+            }
         }
-        viewModel.getTopRatedMovies()
+    }
 
+    private fun setupRecyclerViews() = binding.apply {
+        rvPopularMovies.adapter = popularMoviesRecyclerAdapter
+        rvTopRatedMovies.adapter = topRatedMoviesRecyclerAdapter
+        rvUpcomingMovies.adapter = upcomingMoviesRecyclerAdapter
+    }
+
+    private fun observeMovies() = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.topRatedMoviesLiveData.observe(viewLifecycleOwner) { articles ->
+                topRatedMoviesRecyclerAdapter.submitList(articles)
+            }
+
+            viewModel.popularMoviesLiveData.observe(viewLifecycleOwner) { articles ->
+                popularMoviesRecyclerAdapter.submitList(articles)
+            }
+
+            viewModel.upcomingMoviesLiveData.observe(viewLifecycleOwner) { articles ->
+                upcomingMoviesRecyclerAdapter.submitList(articles)
+            }
+        }
     }
 
     override fun onDestroyView() {

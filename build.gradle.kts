74555d449c2fe9d8c76e364294bbc9f4a73c1085
Bedirhan Tong <bedirhantongdev@gmail.com>
2024-07-22 14:55:14 +0300

    login authentication is modified

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,12 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
 buildscript {
+    repositories {
+        google()
+    }
     dependencies {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+        val navVersion = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
